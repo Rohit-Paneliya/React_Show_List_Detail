1095b9054c3568ee2ddcec4d0bf35ed502f5adf1
Rohit Paneliya <rohit.paneliya@eviden.com>
2024-03-12 11:55:21 +0530

    lottie unused code removed

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,8 @@ import { ThemeProvider } from './utils/ContextProviderThemes';
 
 function App() {
 
-  // const defaultOptions = {
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: animationData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: 'xMidYMid slice'
-  //   }
-  // };
-
-  // const errorBoundryUI = () => {
-  //   return (<div>
-  //     {/* <Lottie options={defaultOptions}
-  //       height={400}
-  //       width={400} /> */}
-  //       <Lottie animationData={defaultOptions} loop={true} />
-  //     <h1>Error while fetching the data.........!!</h1>
-  //   </div>)
-  // }
-
-
   return (
     <ErrorBoundary fallback={<div><h1>Error Boundry.....!!</h1></div>}>
-     {/* <ErrorBoundary fallback={errorBoundryUI()}> */}
       <div>
         <ThemeProvider>
         <HomeScreen />
